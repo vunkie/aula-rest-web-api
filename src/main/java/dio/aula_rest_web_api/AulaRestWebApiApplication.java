0cc8a28b641f5d29335385a3b94c54e2782ca9e8
Matheus Silva <matheus.vunk@gmail.com>
2024-08-20 15:33:53 -0300

    first commit

---
+++ src/main/java/dio/aula_rest_web_api/AulaRestWebApiApplication.java
@@ -0,0 +1,13 @@
+package dio.aula_rest_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaRestWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaRestWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/aula_rest_web_api/AulaRestWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.aula_rest_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaRestWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
